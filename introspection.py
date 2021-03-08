2ac3ec5be5f6a1c23ef310bbc1a62d67f0dc9408
Sam-BRS <sam@bouras.fr>
2021-03-08 21:27:31 +0100

    Create introspection.py

---
+++ introspection.py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import LinearRegression
+
+#check the type of LinearRegression
+print(type(LinearRegression))#abc.ABCMeta
+
+#dir to check the the attribute and method of the object Linear Regression
+print(dir(LinearRegression))
+print(().__doc__)
+print(().__class__)
